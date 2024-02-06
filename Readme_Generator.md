bff428a5d8999be7c110c590f84eb061be2ed0c5
Bisola <bisola@equally.ai>
2024-02-06 15:01:08 +0100

    add template

---
+++ Readme_Generator.md
@@ -0,0 +1,21 @@
+
+# Readme Generator
+
+## About this Project
+A node app to generate readme files from the command line
+
+### Table of content
+
+1.Installation(#installation)
+2.Usage(#usage)
+
+### Instalation
+clone the repo and then run npm install
+
+### Usage
+clone the repo and then run npm install
+
+
+**Additional Resource**
+[Here is a walkthrough video for the app](www.youtube.com)
+
--- index.js
+++ index.js
@@ -1,2 +1,128 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const prompts = inquirer.createPromptModule();
+
+const Questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+    validate: (value) => {
+      if (!value) {
+        return "Please enter a value to continue";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    message: "Give a description of your project",
+    name: "description",
+    validate: (value) => {
+      if (!value) {
+        return "Please enter a value to continue";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    message: "describe how your project can be installed",
+    name: "installation",
+    validate: (value) => {
+      if (!value) {
+        return "Please enter a value to continue";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    message: "describe how your project can be used",
+    name: "usage",
+    validate: (value) => {
+      if (!value) {
+        return "Please enter a value to continue";
+      }
+      return true;
+    },
+  },
+  {
+    type: "confirm",
+    message:
+      "do you have any external resources (videos, images) that be further used to describle",
+    name: "has_external_link",
+    validate: (value) => {
+      if (!value) {
+        return "Please enter a value to continue";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    message: "describe the the type of resource you want to link",
+    name: "external_link_text",
+    validate: (value) => {
+      if (!value) {
+        return "Please enter a value to continue";
+      }
+      return true;
+    },
+    when: (answers) => answers.has_external_link,
+  },
+  {
+    type: "input",
+    message: "enter the link to your resource",
+    name: "external_link",
+    validate: (value) => {
+      if (!value) {
+        return "Please enter a value to continue";
+      }
+      return true;
+    },
+    when: (answers) => answers.external_link_text,
+  },
+];
+
+prompts(Questions).then((answers) => {
+  const template = generateTemplate(answers);
+  createFile(answers.title, template);
+});
+
+const generateTemplate = (options) => `
+# ${options.title}
+
+## About this Project
+${options.description}
+
+### Table of content
+
+1.Installation(#installation)
+2.Usage(#usage)
+
+### Instalation
+${options.installation}
+
+### Usage
+${options.installation}
+
+${
+  options.has_external_link
+    ? `
+**Additional Resource**
+[${options.external_link_text}](${options.external_link})
+`
+    : ""
+}
+`;
+
+const createFile = (fileName, template) => {
+  fs.writeFile(`./${fileName.split(" ").join("_")}.md`, template, (err) => {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log("File generated successfully");
+  });
+};
